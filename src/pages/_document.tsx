282f9b7ef6c848c539b8a1ee1b8c0be7a49e8aef
Marcus Lilja <marcus.lilja@gmail.com>
2022-01-27 00:33:33 +0100

    feature: add inter font

---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+export default class NodlyDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const initialProps = await Document.getInitialProps(context);
+
+    return initialProps;
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html className="tabular-nums antialiased">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="min-h-screen bg-white text-gray-900">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
