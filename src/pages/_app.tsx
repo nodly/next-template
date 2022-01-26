74b22b6aac85841299c9c30f35b7a4665f63e875
Marcus Lilja <marcus.lilja@gmail.com>
2022-01-27 00:16:29 +0100

    fix: change app name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import '@/styles/global.css';
 import type { AppProps } from 'next/app';
 
-function NextApp({ Component, pageProps }: AppProps) {
+function NodlyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default NextApp;
+export default NodlyApp;
