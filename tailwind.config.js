1f0d35fad78e463908c75c201a3794dc5f46d440
Marcus Lilja <marcus.lilja@gmail.com>
2023-04-26 09:44:27 +0200

    chore: bump packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ const config = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
   ],
 };
