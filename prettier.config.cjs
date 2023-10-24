dba39111c26f2762a2ff97ced07fbfcf95ccae9b
Marcus Lilja <marcus.lilja@gmail.com>
2023-10-24 13:21:09 +0200

    feat: add clsx to prettier config for tailwind

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,7 @@
-module.exports = {
+const config = {
   ...require('@nodly/prettier-config'),
   plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx'],
 };
+
+module.exports = config;
