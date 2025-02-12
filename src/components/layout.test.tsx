daeba2c5e23bd60ca7abf5af1f9a6324ffb2173f
Marcus Lilja <marcus.lilja@gmail.com>
2025-02-12 15:15:44 +0100

    fix: incorrect import in test

--- src/components/layout.test.tsx
+++ src/components/layout.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import { Layout } from '../layout';
+import { Layout } from './layout';
 
 describe('Layout component', () => {
   it('should render empty layout', () => {
