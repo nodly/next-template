379eab8b0f06ca9899102ffd723a1087465ca832
Marcus Lilja <marcus.lilja@gmail.com>
2026-02-04 13:48:43 +0100

    fix: disable headers

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,31 @@
 import type { NextConfig } from 'next';
 
-const securityHeaders = [
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload',
-  },
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block',
-  },
-  {
-    key: 'X-Frame-Options',
-    value: 'SAMEORIGIN',
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
-  },
-];
+// const securityHeaders = [
+//   {
+//     key: 'X-DNS-Prefetch-Control',
+//     value: 'on',
+//   },
+//   {
+//     key: 'Strict-Transport-Security',
+//     value: 'max-age=63072000; includeSubDomains; preload',
+//   },
+//   {
+//     key: 'X-XSS-Protection',
+//     value: '1; mode=block',
+//   },
+//   {
+//     key: 'X-Frame-Options',
+//     value: 'SAMEORIGIN',
+//   },
+//   {
+//     key: 'X-Content-Type-Options',
+//     value: 'nosniff',
+//   },
+//   {
+//     key: 'Referrer-Policy',
+//     value: 'origin-when-cross-origin',
+//   },
+// ];
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -34,12 +34,12 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
 
-  headers: async () => [
-    {
-      source: '/:path*',
-      headers: securityHeaders,
-    },
-  ],
+  // headers: async () => [
+  //   {
+  //     source: '/:path*',
+  //     headers: securityHeaders,
+  //   },
+  // ],
 };
 
 export default nextConfig;
